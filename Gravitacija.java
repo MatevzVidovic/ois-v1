4b1a7c53eb13e862f1ad06ca7ba8892ca6b713fe
PetjaSmirnov <79159412+PetjaSmirnov@users.noreply.github.com>
2021-02-24 23:12:09 +0100

    izpis nadmorke in pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -5,4 +5,11 @@ public class Gravitacija {
 	public static void main (String[] args) {
 		System.out.print("OIS je zakon!");
 	}
+
+	public static void izpis(double nadmorskaVisina, double gravitacijskiPospesek) {
+		
+		System.out.println(nadmorskaVisina);
+		System.out.println(gravitacijskiPospesek);
+	}
+
 }
