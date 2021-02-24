443ae031f9f23c4c0347e6ce9a0f8c9d29e78543
SebastjanK <sk0506@student.uni-lj.si>
2021-02-24 23:02:23 +0100

    #3: Dodajanje izračuna pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -5,4 +5,11 @@ public class Gravitacija {
 	public static void main (String[] args) {
 		System.out.print("OIS je zakon!");
 	}
+	
+	private static double gravPospesek(double nadmorskaVisina) {
+		
+		double pospesek = (6.674 * 5.972 * Math.pow(10, 13)) / Math.pow((6371000.0 + nadmorskaVisina), 2)
+		
+		return pospesek;
+	}
 }
